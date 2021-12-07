34706fb051647cb36c6b79328c71c712278fa896
Olga Egortseva <olgaegortseva@gmail.com>
2021-12-07 18:33:56 +0300

    add hooks

--- webdriver-test.js
+++ webdriver-test.js
@@ -1,22 +1,30 @@
+/* eslint-disable max-len */
 const {Builder, By, Key, until} = require('selenium-webdriver');
 const assert = require('chai').assert;
 const {expect} = require('chai');
-describe('select vacuum cleaner in onliner shop scenario', function() {
-  it('should open order form', function() {
-   ( async function example() {
-    const driver = await new Builder().forBrowser('chrome').build();
-    await driver.get('https://catalog.onliner.by/'); 
-    const selectAppliances = await driver.findElement(By.css("[data-id='3']"));
+let driver;
+
+describe('Onliner catalog tests', async function() {
+  this.timeout(30000);
+  beforeEach(async () => {
+    driver = await new Builder().forBrowser('chrome').build();
+  });
+  afterEach(async () => {
+    await driver.quit();
+  });
+  it('should open order form', async function() {
+    await driver.get('https://catalog.onliner.by/');
+    const selectAppliances =  driver.findElement(By.css('[data-id=\'3\']'));
     await selectAppliances.click();
-    const selectCleaning = await driver.findElement(By.xpath("//*[contains(text(),'Уборка')]"));
-    const actions = driver.actions({ async:true });
-    await actions.move({ origin: selectCleaning }).perform();
-    const selectRobotVacuumCleaners = await driver.findElement(By.xpath("//*[contains(@class,'catalog-navigation-list__aside-item_active')]//span[contains(text(),'Роботы-пылесосы')]"));
+    const selectCleaning =  driver.findElement(By.xpath('//*[contains(text(),\'Уборка\')]'));
+    const actions = driver.actions({async: true});
+    await actions.move({origin: selectCleaning}).perform();
+    const selectRobotVacuumCleaners = await driver.findElement(By.xpath('//*[contains(@class,\'catalog-navigation-list__aside-item_active\')]//span[contains(text(),\'Роботы-пылесосы\')]'));
     await selectRobotVacuumCleaners.click();
-    let selectTypeFloor = await driver.findElement(By.xpath("//input[@value='floor']/.."));
+    const selectTypeFloor = await driver.findElement(By.xpath('//input[@value=\'floor\']/..'));
     await driver.executeScript('arguments[0].scrollIntoView();', selectTypeFloor);
     await selectTypeFloor.click();
-    const selectRoborockProducer = driver.findElement(By.xpath("//ul[@class='schema-filter__list']//input[@value='roborock']/.."));
+    const selectRoborockProducer = driver.findElement(By.xpath('//ul[@class=\'schema-filter__list\']//input[@value=\'roborock\']/..'));
     await driver.executeScript('arguments[0].scrollIntoView();', selectRoborockProducer);
     await selectRoborockProducer.click();
     const goodsCounter = await driver.findElement(By.css('.schema-filter-button__state_control'));
@@ -29,12 +37,11 @@ describe('select vacuum cleaner in onliner shop scenario', function() {
     await driver.wait(until.elementIsVisible(confirmationButton), 5000);
     await confirmationButton.click();
     const buyNowButton = await driver.findElement(By.css('.offers-list__item:first-child .offers-list__control.offers-list__control_default.helpers_hide_tablet [href]'));
-    await buyNowButton.click(); 
+    await buyNowButton.click();
     await driver.wait(until.elementLocated(By.css('.cart-form__title_extended-alter')), 5000);
     const orderFormTitle = await driver.findElement(By.css('.cart-form__title_extended-alter'));
     assert.isTrue(await orderFormTitle.isDisplayed());
-    expect(await orderFormTitle.getText()).to.include('Оформление заказа');  
-    await driver.quit();
-})();
+    expect(await orderFormTitle.getText()).to.include('Оформление заказа');
+  });
 });
-});
+
